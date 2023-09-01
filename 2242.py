8bec30804d685eb12d5dec3dae2071bbcd82f9aa
Carlos Eduardo <caedloureiro@hotmail.com>
2023-09-01 11:52:42 -0400

    Update 2242.py

--- 2242.py
+++ 2242.py
@@ -1,6 +1,20 @@
 vogais = 'aeiou'
 
 frase = input()
+lista = []
 
-for i in range(len(frase)):
-    if ()
+for letter in frase:
+    if letter in vogais:
+        lista.append(letter)
+
+tam = len(lista)
+eng = True
+
+for i in range(tam):
+    if lista[i] != lista[tam - i - 1]:
+        print("N")
+        eng = False
+        break
+
+if (eng):
+    print("S")
