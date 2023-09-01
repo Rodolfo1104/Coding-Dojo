638a04907e172452154575b853ddf1c307d9284b
AlejandroRL <alejandroloureiro2000@gmail.com>
2023-09-01 10:11:23 -0400

    Create 1551.py

---
+++ 1551.py
@@ -0,0 +1,18 @@
+N = int(input())
+letras = 'abcdefghijklmnopqrstuvwxyz'
+for i in range(N):
+    frase = input().lower()
+    count = 0
+    for letra in letras:
+        if letra in frase:
+            count += 1
+    
+
+    if (count  == 26):
+        print("frase completa")
+    elif(count >= 13):
+        print("frase quase completa")
+    else:
+        print("frase mal elaborada")
+    
+    
