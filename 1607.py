594d481247a197a8ec5eb980ae45ab9a3b10fd5d
Geovana Matias R <119976015+Rodolfo1104@users.noreply.github.com>
2023-09-01 11:25:30 -0400

    Update 1607.py

--- 1607.py
+++ 1607.py
@@ -4,14 +4,16 @@ for i in range(T):
     cont = 0
     A, B = input().split(' ')
     for j in range(len(A)):
+
         if A[j] == B[j]:
             continue
         else:
+            ord(A[j]) - 
             while A[j] != B[j]:
                 if (A[j] == 'z'):
                     A[j] = 'a'                   
                 else:
-                    num_letra = ord(A[j]) + 1
+                    
                     A[j] = chr(num_letra)
                 cont += 1
             
