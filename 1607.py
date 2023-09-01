841ecb4fad7bd0ef095fbae690c353a75b5884cd
AlejandroRL <alejandroloureiro2000@gmail.com>
2023-09-01 11:09:52 -0400

    aa
    aa

--- 1607.py
+++ 1607.py
@@ -2,5 +2,16 @@ T = int(input())
 
 for i in range(T):
     A, B = input().split(' ')
-    for j in range(len)
+    for j in range(len(A)):
+        if A[j] == B[j]:
+            continue
+        else:
+            if (A[j] == 'z'):
+                A[j] = 'a'
+            else:
+                num_letra = ord(A) + 1
+                A[j] = 
+            
+            
+        
     
