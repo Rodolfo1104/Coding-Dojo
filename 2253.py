64d505b164d57ac724e741a1f88c1eeec76a2e9b
Rodolfo1104 <rodolfosimoesmakarem@gmail.com>
2023-09-01 12:06:41 -0400

    Update 2253.py

--- 2253.py
+++ 2253.py
@@ -1,7 +1,26 @@
 while True:
     try:
-        senha = split()
+        senha = input()
+        alp = False
+        min = False
+        mai = False
+        num = False
 
+        if senha.isalnum():
+            alp = True
+
+        for letter in senha:
+            if letter.isupper():
+                mai = True
+            if letter.islower():
+                min = True
+            if letter.isnumeric():
+                num = True
+
+        if alp and min and mai and num:
+            print("Senha valida.")
+        else:
+            print("Senha invalida.")
 
     except EOFError:
         break
