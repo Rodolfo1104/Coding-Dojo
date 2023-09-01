33326c2c9afecd556ecbb96782384c3219834e60
William Massami <99924347+h4rad4@users.noreply.github.com>
2023-09-01 10:43:23 -0400

    Update 1581.py

--- 1581.py
+++ 1581.py
@@ -1,9 +1,9 @@
 n = int(input())
 for i in range(n):
+
     k = int(input())
     lingua = input()
     for j in range(k-1):
         if lingua != input():
             lingua = "ingles"
-            break
     print(lingua)
